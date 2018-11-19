6f0da96773236dfc4e419820cf7f70ad44cd8480
Willy LE ROY <w.leroy@optline.fr>
2018-11-19 10:08:40 +0100

    Improved test script - use captured data or a pre-defined array of values

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+file=dump.data
 
-clear; echo; echo; ./emulate_stream.py | ./SynScanCoords.py
+# Use actual captured data
+if [ "$1" == "file" ] && [ -f $file ]; then
+	clear; echo; echo; cat $file | ./SynScanCoords.py
+else
+
+	# Use a loop of fake data
+	clear; echo; echo; ./emulate_stream.py | ./SynScanCoords.py
+fi
