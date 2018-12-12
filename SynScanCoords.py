eb8b4381e568c69bff48dea66ef18856bd561110
Willy LE ROY <wlr86fr+github@gmail.com>
2018-12-12 23:09:12 +0100

    First test using actual LCD display

--- SynScanCoords.py
+++ SynScanCoords.py
@@ -1,9 +1,80 @@
 #! /usr/local/bin/python3
 # vim: set fileencoding=utf-8 autoindent expandtab tabstop=2 shiftwidth=2 softtabstop=2 :
 
-import string, time, sys, re
+import string, time, sys, re, smbus
 
 CHUNK_SIZE = 20
+
+# Define some device parameters
+I2C_ADDR  = 0x27 # I2C device address, if any error, change this address to 0x3f
+LCD_WIDTH = 16   # Maximum characters per line
+
+# Define some device constants
+LCD_CHR = 1 # Mode - Sending data
+LCD_CMD = 0 # Mode - Sending command
+
+LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
+LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
+LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
+
+LCD_BACKLIGHT  = 0x08  # On
+#  LCD_BACKLIGHT = 0x00  # Off
+
+ENABLE = 0b00000100 # Enable bit
+
+# Timing constants
+E_PULSE = 0.0005
+E_DELAY = 0.0005
+
+#Open I2C interface
+#bus = smbus.SMBus(0)  # Rev 1 Pi uses 0
+bus = smbus.SMBus(1) # Rev 2 Pi uses 1
+
+def lcd_init():
+  # Initialise display
+  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
+  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
+  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
+  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off 
+  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
+  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
+  time.sleep(E_DELAY)
+
+def lcd_byte(bits, mode):
+  # Send byte to data pins
+  # bits = the data
+  # mode = 1 for data
+  #        0 for command
+
+  bits_high = mode | (bits & 0xF0) | LCD_BACKLIGHT
+  bits_low = mode | ((bits<<4) & 0xF0) | LCD_BACKLIGHT
+
+  # High bits
+  bus.write_byte(I2C_ADDR, bits_high)
+  lcd_toggle_enable(bits_high)
+
+  # Low bits
+  bus.write_byte(I2C_ADDR, bits_low)
+  lcd_toggle_enable(bits_low)
+
+def lcd_toggle_enable(bits):
+  # Toggle enable
+  time.sleep(E_DELAY)
+  bus.write_byte(I2C_ADDR, (bits | ENABLE))
+  time.sleep(E_PULSE)
+  bus.write_byte(I2C_ADDR,(bits & ~ENABLE))
+  time.sleep(E_DELAY)
+
+def lcd_string(message,line):
+  # Send string to display
+
+  message = message.ljust(LCD_WIDTH," ")
+
+  lcd_byte(line, LCD_CMD)
+
+  for i in range(LCD_WIDTH):
+    lcd_byte(ord(message[i]),LCD_CHR)
 
 def hex2deg(n=''):
   value = int(int(n,16)/256)
@@ -24,7 +95,7 @@ def deg2HMS(ra='', dec='', round=True):
       decS = int((abs((dec-deg)*60)-decM)*60)
     else:
       decS = (abs((dec-deg)*60)-decM)*60
-    DEC = "{}{:02d}°{:02d}'{:02d}\"".format(ds, deg, decM, decS)
+    DEC = "{}{:02d}ß{:02d}'{:02d}\"".format(ds, deg, decM, decS)
   if ra:
     if str(ra)[0] == '-':
       rs, ra = '-', abs(ra)
@@ -58,9 +129,14 @@ def displayConsole(ra='',dec='',labels='short'):
   sys.stdout.write( u"\u001b[2A" )
   sys.stdout.write( u"\u001b[30D" )
   # This timer is aimed at slowing down the output when simulating data from a
-  # dump file - Remove this on
-  time.sleep(0.25)
+  # dump file - Remove this on production
+  time.sleep(0.05)
   sys.stdout.flush()
+
+def displayLCD(ra='',dec='',labels='short'):
+
+    lcd_string(' ' + ra,LCD_LINE_1)
+    lcd_string(dec,LCD_LINE_2)
 
 def each_chunk(stream, separator):
   buffer = ''
@@ -97,6 +173,7 @@ def loopDecode():
     print("Dec:",decode(dec=hexDec))
 
 if __name__ == '__main__':
+  lcd_init()
   myFile = sys.stdin
   for chunk in each_chunk(myFile, separator='#'):
     string = re.compile(r"e([A-F0-9\/]{8})\,([A-F,0-9]{8})")
@@ -108,6 +185,7 @@ if __name__ == '__main__':
       Dec    = decode(dec=hexDec)
       #  print( ' RA:' + str(RA)  )
       #  print( 'Dec:' + str(Dec) )
-      displayConsole(ra=RA,dec=Dec,labels='long')
+      #  displayConsole(ra=RA,dec=Dec,labels='long')
+      displayLCD(ra=RA,dec=Dec,labels='long')
 
 
