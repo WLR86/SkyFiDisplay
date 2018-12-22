6a74ec3a25975b1d82306805b0aee4219ec607f4
WLR86 <wlr86fr+github@gmail.com>
2018-12-22 14:50:02 +0100

    Started to implement Standalone mode - not working yet

--- service.py
+++ service.py
@@ -4,30 +4,41 @@ import subprocess,os,time
 
 from display import *
 
+modeFile = '/tmp/mode'
+snoopRXTXCmd = 'tailf /tmp/tb | /home/pi/SkyiDisplay/SynScanCoords.py'
+
 def main():
     lcd_init()
-    lcd_string("SkyFi+DSC Disp",LCD_LINE_1)
-    lcd_string("(C) 2018 Weetos",LCD_LINE_2)
+    lcd_string('SkyFi+DSC Disp ', LCD_LINE_1)
+    lcd_string('(C) 2018 Weetos', LCD_LINE_2)
     time.sleep(5)
     while True:
         # set an interrupt on a falling edge and wait for it to happen
         # GPIO.wait_for_edge(INT, GPIO.FALLING)
 
-        exists = os.path.isfile('/tmp/tb')
-        if exists:
-            # Store configuration file values
-            subprocess.call(['tailf /tmp/tb | /home/pi/SkyFiDisplay/SynScanCoords.py'], shell=True, \
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        # In the meantime, let's do it without using GPIO
+        from pathlib import Path
+        mode = Path(modeFile).read_text()
 
+        if mode == 'standalone':
+           # Testing
+           lcd_string('Standalone mode ', LCD_LINE_1)
+           lcd_string('              ON', LCD_LINE_2)
         else:
-            time.sleep(2)
-            #  print('waiting...')
-            lcd_string("Please Connect",LCD_LINE_1)
-            lcd_string("Client App",LCD_LINE_2)
-            time.sleep(4)
-            lcd_string("Waiting",LCD_LINE_1)
-            lcd_string("for data ...",LCD_LINE_2)
+           lcd_string('AppDriven mode  ', LCD_LINE_1)
+           lcd_string('              ON', LCD_LINE_2)
+           time.sleep(2)
+           exists = os.path.isfile('/tmp/tb')
+           if exists:
+                subprocess.call([snoopRXTXCmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+           else:
+                time.sleep(2)
+                lcd_string('Please Connect  ', LCD_LINE_1)
+                lcd_string('Client App      ', LCD_LINE_2)
+                time.sleep(4)
+                lcd_string('Waiting         ', LCD_LINE_1)
+                lcd_string('for data ...    ', LCD_LINE_2)
 
 if __name__ == '__main__':
     main()
